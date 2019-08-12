f4be700c84084ae86ac1bfc4209fcae6d527c614
fr-me-jim <fr.me.jim@gmail.com>
2019-08-12 19:49:03 +0200

    + AddProduct content

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -2,7 +2,105 @@ import React from 'react';
 
 const AddProduct = () => {
     return (  
-        <h1 className="text-center">Add New Product</h1>
+        <div className="col-md-8 mx-auto ">
+            <h1 className="text-center">Add New Product</h1>
+
+            <form
+                className="mt-5"
+            >
+                <div className="form-group">
+                    <label>Dish Name</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        name="name" 
+                        placeholder="Dish Name"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Dish Price</label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        name="price"
+                        placeholder="Dish Price"
+                    />
+                </div>
+
+                <legend className="text-center">Category:</legend>
+                <div className="text-center">
+                    <div className="form-check form-check-inline">
+                        <input 
+                            id="dessert"
+                            className="form-check-input" 
+                            type="radio" 
+                            name="category"
+                            value="dessert"
+                        />
+                        <label 
+                            htmlFor="dessert"
+                            className="form-check-label"
+                        >
+                            Dessert
+                        </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input 
+                            id="drink"
+                            className="form-check-input" 
+                            type="radio" 
+                            name="category"
+                            value="drink"
+                        />
+                        <label 
+                            htmlFor="drink"
+                            className="form-check-label"
+                        >
+                            Drink
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input 
+                            id="cuts"
+                            className="form-check-input" 
+                            type="radio" 
+                            name="category"
+                            value="cuts"
+                        />
+                        <label 
+                            htmlFor="cuts"
+                            className="form-check-label"
+                        >
+                            Cuts
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input 
+                            id="salat"
+                            className="form-check-input" 
+                            type="radio" 
+                            name="category"
+                            value="salat"
+                        />
+                        <label 
+                            htmlFor="salat"
+                            className="form-check-label"
+                        >
+                            Salat
+                        </label>
+                    </div>
+                </div>
+
+                <input 
+                    type="submit" 
+                    className="font-weight-bold text-uppercase mt-5 btn btn-info btn-block py-3" 
+                    value="Add Product" 
+                />
+            </form>
+        </div>
     );
 }
  
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const ProductList = ({product}) => {
+
+    const handleClick = id => {
+        console.log(id);
+        //TODO : delete resgidtry   
+    }
+
     return (  
         <li 
             data-category={product.category} 
@@ -12,6 +20,16 @@ const ProductList = ({product}) => {
             </p>
 
             <div>
+                <Link 
+                    to={`/products/edit/${product.id}`}
+                    className="btn btn-success mr-2"
+                > Edit </Link>
+
+                <button 
+                    type="submit"
+                    className="btn btn-danger"
+                    onClick={ () => handleClick(product.id) }
+                > Delete &times; </button>
 
             </div>
         </li>
