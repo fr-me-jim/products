17f1e5ee5e21c7812565b1b43a522da93a6ba8e6
fr-me-jim <fr.me.jim@gmail.com>
2019-08-12 18:28:50 +0200

    finished header + db.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from './components/Header';
 import ProductsList from './components/ProductsList';
@@ -8,16 +9,24 @@ import EditProduct from './components/EditProduct';
 import Product from './components/Product';
 
 function App() {
+
+  //state
+  const [ products, setProducts ] = useState([]);
+
   return (
     <Router>
       <Header />
 
-      <Switch>
-        <Route exact path="/new-product" component={AddProduct} />
-        <Route exact path="/products" component={ProductsList} />
-        <Route exact path="/products/edit/:id" component={EditProduct} />
-        <Route  exact path="/products/:id" component={Product} />
-      </Switch>
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/products/new" component={AddProduct} />
+          <Route exact path="/products" component={ProductsList} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+          <Route  exact path="/products/:id" component={Product} />
+        </Switch>
+      </main>
+
+      <p className="mt-4 p-2 text-center">All rights reserved</p>
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,22 @@ const Header = () => (
             <Link to="/products" className="navbar-brand" >
                 React CRUD & Routing
             </Link>
+
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <Link
+                        to="/products"
+                        className="nav-link"
+                    >Products</Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link
+                        to="/products/new"
+                        className="nav-link"
+                    >ADD Product</Link>
+                </li>
+            </ul>
         </div>
     </nav>
 );
